087f3c8b7b027573a5a55d645044b23b9d557193
Mamta-skip <mamtaskip25@gmail.com>
2025-12-11 11:21:24 +0545

    feat: featured tours section

---
+++ src/components/home/featured-tours.tsx
@@ -0,0 +1,48 @@
+import { Mountain, User } from 'lucide-react';
+import { Button } from '../ui/button';
+import { Card } from '../ui/card';
+import { Heading, HightlightText, NormalText } from './explore-desination';
+
+export function FeaturedTours() {
+  return (
+    <div className='container my-20'>
+      <div className='space-y-2'>
+        <p className='uppercase text-primary font-xs'>Featured Tours</p>
+        <Heading className='font-semibold '>
+          <NormalText text='Our Best Selling' />
+          <HightlightText text='Adventures' className='text-secondary' />
+        </Heading>
+        <p className='text-muted-foreground '>Handpicked experiences loved by thousands of adventurers worldwide</p>
+      </div>
+      <div className='grid grid-cols-3 gap-6 mt-8'>
+        <TourCard />
+        <TourCard />
+        <TourCard />
+      </div>
+    </div>
+  );
+}
+function TourCard() {
+  return (
+    <Card className='p-0 overflow-hidden'>
+      <img src='/des.jpg' alt='tour' className='w-full h-60 object-cover' />
+      <div className='px-2 pb-2'>
+        <p className='font-semibold mt-2'>Everest Base Camp Trek</p>
+        <p className='flex gap-2'>
+          <p>
+            <Mountain className='inline-block mr-1 mb-1 text-secondary' /> 2-15
+          </p>
+          <p>
+            <User className='inline-block mr-1 mb-1 text-secondary' /> 2-15
+          </p>
+        </p>
+        <div className='flex justify-between items-center'>
+          <p className='font-bold text-lg'> From $1200</p>
+          <Button className='bg-primary text-primary-foreground px-4 py-2 rounded-lg hover:bg-primary/90 transition'>
+            Book Now
+          </Button>
+        </div>
+      </div>
+    </Card>
+  );
+}
