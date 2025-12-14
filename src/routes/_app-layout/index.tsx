c2a2dfaac47b654a5a96cf1718e09f65790a15b4
aryan <aryanmgr79@gmail.com>
2025-12-14 15:27:24 +0545

    feat: hero section

--- src/routes/_app-layout/index.tsx
+++ src/routes/_app-layout/index.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { Destinations } from '@/components/home/explore-desination';
 import { FeaturedTours } from '@/components/home/featured-tours';
+import { Hero } from '@/components/home/hero';
 import { WhyChooseUs } from '@/components/home/why-choose-us';
 
 export const Route = createFileRoute('/_app-layout/')({
@@ -10,6 +11,7 @@ export const Route = createFileRoute('/_app-layout/')({
 function RouteComponent() {
   return (
     <>
+      <Hero />
       <Destinations />
       <FeaturedTours />
       <WhyChooseUs />
